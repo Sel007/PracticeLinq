1bfcfc43e2b9f292d2eda35751013e9d96c1fdd0
Sel007 <selina.08225.1@gmail.com>
2020-07-12 23:43:28 -0400

    Completed

---
+++ PracticeLinqConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+
+namespace PracticeLinqConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Practicing Linq";
+            Console.WriteLine(phrase);
+            Console.WriteLine();
+
+            //Query Syntax
+            //string cars = "Car List";
+            string[] carList = { "Porche", "Tesla", "Land Rover", "Acura", "BMW", "Ferrari" };
+
+            var cars = from car in carList
+                      where car.Contains('r')
+                      orderby car ascending
+                      select car;
+
+            foreach (var r in cars)
+            {
+                Console.WriteLine($"{r}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
